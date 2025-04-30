b0edaecbe85b8d17dbba920f26b7b233c6a26f93
MarcosJVPR <perezmarcosjulio@gmail.com>
2025-04-30 16:40:20 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      let dominio = pronoun[p] + adj[a] + noun[n] + ".com";
+      console.log(dominio);
+    }
+  }
+}
+
+function domName (pronoun, adj, noun){
+for (let p = 0; p < pronoun.length; p++){
+  for (let a = 0; a < adj.length; a++){
+    for (let n = 0; n < noun.length; n++){
+        console.log (pronoun[p] + adj[a] + noun[n] + ".net")
+    }
+  }
+}
+}
+domName(pronoun, adj, noun)
