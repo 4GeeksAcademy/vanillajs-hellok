4ba4aa533388f084802ca127e46239e1f3fb22c3
MarcosJVPR <perezmarcosjulio@gmail.com>
2025-05-05 18:46:44 +0000

    tarea

--- src/app.js
+++ src/app.js
@@ -21,14 +21,3 @@ for (let p = 0; p < pronoun.length; p++) {
     }
   }
 }
-
-function domName (pronoun, adj, noun){
-for (let p = 0; p < pronoun.length; p++){
-  for (let a = 0; a < adj.length; a++){
-    for (let n = 0; n < noun.length; n++){
-        console.log (pronoun[p] + adj[a] + noun[n] + ".net")
-    }
-  }
-}
-}
-domName(pronoun, adj, noun)
